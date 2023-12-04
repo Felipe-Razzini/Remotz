9534de5c8c6409fe151082d3a3c79e41fa7ead61
Felipe Razzini <feliperazzini95@gmail.com>
2023-12-04 12:59:35 +0100

    destroy logic - DONE

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,15 +1,14 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
-
+  skip_before_action :authenticate_user!, only: [:home]
+  helper_method :dashboard_page?
 
   def home
   end
 
   def dashboard
     @projects = Project.includes(:user).all
-    puts @projects
-    @tasks_today = Task.where('DATE(start) = ?', Date.today)
     @projects_status_count = Project.group(:status).count
     @projects_workload_count = Project.joins(:user).group(:username).count
+    @tasks_today = Task.where('DATE(start) = ?', Date.today)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :projects
-  has_many :tasks, through: :projects
+  has_many :tasks
   has_many :alerts, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
