e472bce87a5181a2e6bda588faccdb341df46090
Felipe Razzini <feliperazzini95@gmail.com>
2023-12-04 22:46:11 +0100

    create and destroy alerts - done

--- app/javascript/controllers/alert_controller.js
+++ app/javascript/controllers/alert_controller.js
@@ -1,16 +1,16 @@
 import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="add"
+// Connects to data-controller="alert"
 export default class extends Controller {
   static targets = ["form"]
-
+  
   connect() {
     console.log(this.formTarget);
   }
 
   displayForm(e) {
-    e.preventDefault(e);
+    e.preventDefault();
 
-    this.formTarget.classList.remove("d-none");
+    this.formTarget.classList.remove('d-none');
   }
 }
