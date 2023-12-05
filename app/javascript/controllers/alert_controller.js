db23ae3c77b20dc9bbc286c218c749b9960b7809
Felipe Razzini <feliperazzini95@gmail.com>
2023-12-05 14:54:03 +0100

    fake-alert

--- app/javascript/controllers/alert_controller.js
+++ app/javascript/controllers/alert_controller.js
@@ -2,15 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="alert"
 export default class extends Controller {
-  static targets = ["form"]
-  
+  static targets = ["overlay"]
+
   connect() {
     console.log(this.formTarget);
   }
 
-  displayForm(e) {
-    e.preventDefault();
+  // displayForm(e) {
+  //   e.preventDefault();
 
-    this.formTarget.classList.remove('d-none');
+  //   this.formTarget.classList.remove('d-none');
+  // }
+
+  displayAlert() {
+    this.overlayTarget.classList.add("fadeIn")
+    this.overlayTarget.classList.remove("hidden")
+
+    setTimeout(() => {
+      this.overlayTarget.classList.add("fadeOut")
+      this.overlayTarget.classList.add("hidden")
+    }, 4000);
   }
 }
