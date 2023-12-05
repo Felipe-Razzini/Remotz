7dee0faa606a5f3a799332213494288653c521e1
Felipe Razzini <feliperazzini95@gmail.com>
2023-12-05 10:24:37 +0100

    cloudinary setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   has_many :alerts, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_many :chatrooms, through: :messages
+  has_one_attached :photo
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
