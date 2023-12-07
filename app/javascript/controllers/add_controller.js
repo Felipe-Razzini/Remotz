9139def587906d5b7a75ca09297f2fce72d3e948
Robert Chheav <robert.chheav@gmail.com>
2023-12-07 10:02:29 +0100

    minor changes on sidebar and planner page

--- app/javascript/controllers/add_controller.js
+++ app/javascript/controllers/add_controller.js
@@ -2,9 +2,10 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="add"
 export default class extends Controller {
-  static targets = ["form"]
+  static targets = ["form", "pageName"]
 
   connect() {
+    this.load();
     console.log(this.formTarget);
   }
 
@@ -19,4 +20,20 @@ export default class extends Controller {
 
     this.formTarget.classList.add('d-none')
   }
+
+  load() {
+      const currentPath = window.location.pathname;
+      const pages = this.pageNameTargets;
+
+      pages.forEach(page => {
+        const pagePath = page.dataset.page;
+
+        if (pagePath === currentPath) {
+          page.classList.add("active-tab");
+        }
+        else {
+          page.classList.remove("active-tab");
+        }
+      });
+    }
 }
